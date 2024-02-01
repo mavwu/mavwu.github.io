65493379ecfedc8cb5e45f2c775b122a05703a8b
Alfiex49 <137775523+Alfiex49@users.noreply.github.com>
2024-02-01 17:07:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetSection = document.querySelector(this.getAttribute('href'));
+
+        if(targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
