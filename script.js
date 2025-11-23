41c02ed2c3c16969632d496c481dbdfc038b5c43
mavwu <viwaplayz@gmail.com>
2025-11-23 04:46:25 +0200

    A COMPLETE REFINEMENT AND REVAMP
    - deleted old files that were not being used
    - removed javascript code from index.html
    - refined the site background to regular white
    - added a new image to site (cartoonized picture)
    - refined the the html, css and js file to a more modern look

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
 
-        const targetSection = document.querySelector(this.getAttribute('href'));
-
-        if(targetSection) {
-            targetSection.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
+    // ===== Smooth scroll for all anchor links =====
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetSection = document.querySelector(this.getAttribute('href'));
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
     });
 });
